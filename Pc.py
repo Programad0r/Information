34072537024591ec78d9da50bb1527596aa51cf1
Gabriel Rufo Macena <61637079+Programad0r@users.noreply.github.com>
2022-06-12 23:56:20 -0300

    Add files via upload

---
+++ Pc.py
@@ -0,0 +1,12 @@
+import wmi
+
+c = wmi.WMI()
+
+my_system = c.Win32_ComputerSystem()[0]
+
+print(f"Marca: {my_system.Manufacturer}")
+print(f"Modelo: {my_system.Model}")
+print(f"Nome: {my_system.Name}")
+print(f"Qtde. CPUs: {my_system.NumberOfProcessors}")
+print(f"Arquitetura: {my_system.SystemType}")
+print(f"Família: {my_system.SystemFamily}")
